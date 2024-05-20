305fe8f0760fd77acfc25219be9ae398a74f16e1
sanjeevitha <sanj@gmail.com>
2024-05-20 14:51:34 +0530

    commiting

--- question7.py
+++ question7.py
@@ -1,10 +1,22 @@
 # create a list as below
-output=[[1,2,3],[4,5,6],[7,5,3,2,3,[2,[3]]]]
+#op=[[1,2,3],[4,5,6],[7,5,3,2,3,[2,[3]]]]
 a=[1,2,3]
 b=[4,5,6]
 c=[7,5,3,2,3]
 d=[2]
 e=[3]
+
+output = []
+output.append(a)
+output.append(b)
+
+d.append(e)
+
+c.append(d)
+
+output.append(c)
+
+print(output)
 
 # use these lists to create another list which will look like the list output in line 2
 
