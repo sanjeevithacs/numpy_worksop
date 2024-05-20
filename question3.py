4afb9d36457cf66a8fed8874f5a2fa95a74148c1
sanjeevithacs <151612107+sanjeevithacs@users.noreply.github.com>
2024-05-20 14:25:03 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,11 @@
 #write a program to find the factorial of a nummber
+def fact(n):
+  if n==1 or num==0:
+    return 1
+  else:
+    return n*fact(n-1)
+  n = int(input())
+  if(fact(n)):
+    print(f"{n} is Palindrome")
+  else:
+    print(f"{n} is not a Palindrome")
